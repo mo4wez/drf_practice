e52eab81215d4b0ef6fb019ae17781cf9b9af04d
moawezz <moawez.de@gmail.com>
2023-09-26 21:05:34 +0330

    work with some lookups

--- store/views.py
+++ store/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from .models import Product
 
 def show_data(request):
-    queryset = Product.objects.filter(id=4324)
+    queryset = Product.objects.filter(category__title__icontains='student')
+    print(len(queryset))
 
-    return render(request, 'store/detail.html')
+    return render(request, 'store/detail.html', context={
+        'products': list(queryset),
+    })
