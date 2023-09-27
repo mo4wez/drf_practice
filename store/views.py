87fc2be25b569d7f86b3965233357b69f8d6e236
moawezz <moawez.de@gmail.com>
2023-09-27 15:13:15 +0330

    work with Q object

--- store/views.py
+++ store/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
+from django.db.models import Q
 
 from .models import Product, OrderItem, Customer, Order
 
 def show_data(request):
-    queryset_tom = Customer.objects.filter(first_name__icontains='tom')
-    queryset = Order.objects.filter(customer__in=queryset_tom)
-
+    queryset = Product.objects.filter(~Q(inventory__lt=30))
     print(list(queryset))
 
     return render(request, 'store/detail.html',)
