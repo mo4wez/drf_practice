fd9d98e31058fa8ed345fda24528f542a8591595
moawezz <moawez.de@gmail.com>
2023-09-27 12:41:24 +0330

    word with chaining filters and lookups

--- store/views.py
+++ store/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 from .models import Product
 
 def show_data(request):
-    queryset = Product.objects.filter(category__title__icontains='student')
+    queryset = Product.objects.filter(
+        name__icontains='car',
+        datetime_created__year=2021,
+        inventory__gt=50,
+        )
     print(len(queryset))
 
     return render(request, 'store/detail.html', context={
